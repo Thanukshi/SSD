81703e82e40087f63fccdb6a7f48341fa4b91782
Thanukshi Wickramasinghe <43212582+Thanukshi@users.noreply.github.com>
2021-10-01 14:02:00 +0530

    IT18202632

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ToastContainer
+        position="bottom-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <Router>
+        <Switch></Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
