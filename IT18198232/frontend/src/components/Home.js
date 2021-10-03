e1bac53bb54d870cfaa365a7c5790afec004302f
Mishane Perera <44816836+mish524@users.noreply.github.com>
2021-10-03 20:43:47 +0530

    IT18198232

--- IT18198232/frontend/src/components/Home.js
+++ IT18198232/frontend/src/components/Home.js
@@ -182,11 +182,7 @@ class Home extends Component {
                   </h3>
                 </div>
                 <p className="about-text">
-                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                  accusantium doloremque laudantium, totam rem aperiam, eaque
-                  ipsa quae ab illo inventore veritatis et quasi architecto
-                  beatae vitae dicta sunt explicabo.Nemo enim ipsam voluptatem
-                  quia voluptas sit.
+
                 </p>
                 <a href="about.html" className="button-w3ls mt-5">
                   Read More
