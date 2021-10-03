14b9b1c8a95f257b8375f76e7b3bda84321cd705
Thanukshi Wickramasinghe <43212582+Thanukshi@users.noreply.github.com>
2021-10-03 12:02:42 +0530

    IT18202632

--- backend/index.js
+++ backend/index.js
@@ -140,4 +140,114 @@
      * @param access code
      * 
      * */
-    
+     app.post('/getToken',(req,res)=>{
+        if(req.body.code == null) return res.status(400).send('Invalid Request');
+        oAuth2Client.getToken(req.body.code, (err,token)=>{
+          if(err){
+            console.error('Error retreiving access token',err);
+            return res.status(400).send('Error retreiving access token');
+          }
+          res.send(token);
+        });
+      });
+      
+      /**
+       * 
+       * access the user profile
+       * @param token
+       * 
+       * */
+      app.post('/getUserInfo', (req, res) => {
+        if (req.body.token == null) return res.status(400).send('Token not found');
+        oAuth2Client.setCredentials(req.body.token);
+        const oauth2 = google.oauth2({ version: 'v2', auth: oAuth2Client });
+      
+        oauth2.userinfo.get((err, response) => {
+            if (err) res.status(400).send(err);
+            console.log(response.data);
+            res.send(response.data);
+        })
+      });
+      
+      /**
+       * 
+       * access file from drive & view
+       * @param token
+       * @returns files
+       * 
+       * */ 
+      app.post('/readDrive', (req, res) => {
+        if (req.body.token == null) return res.status(400).send('Token not found');
+        oAuth2Client.setCredentials(req.body.token);
+        const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+        drive.files.list({
+            pageSize: 10,
+        }, (err, response) => {
+            if (err) {
+                console.log('The API returned an error: ' + err);
+                return res.status(400).send(err);
+            }
+            const files = response.data.files;
+            if (files.length) {
+                console.log('Files:');
+                files.map((file) => {
+                    console.log(`${file.name} (${file.id})`);
+                });
+            } else {
+                console.log('No files found.');
+            }
+            res.send(files);
+        });
+      });
+      
+      /**
+       * 
+       * file upload to drive
+       * @param files
+       * @param token
+       * @returns message
+       * 
+       * */
+      app.post('/fileUpload', (req, res) => {
+        var form = new formidable.IncomingForm();
+        form.parse(req, (err, fields, files) => {
+            if (err) return res.status(400).send(err);
+            const token = JSON.parse(fields.token);
+            console.log(token)
+            if (token == null) return res.status(400).send('Token not found');
+            oAuth2Client.setCredentials(token);
+            console.log(files.file);
+            const drive = google.drive({ version: "v3", auth: oAuth2Client });
+            const fileMetadata = {
+                name: files.file.name,
+            };
+            const media = {
+                mimeType: files.file.type,
+                body: fs.createReadStream(files.file.path),
+            };
+            drive.files.create(
+                {
+                    resource: fileMetadata,
+                    media: media,
+                    fields: "id",
+                },
+                (err, file) => {
+                    oAuth2Client.setCredentials(null);
+                    if (err) {
+                        console.error(err);
+                        res.status(400).send(err)
+                    } else {
+                        res.send('Successfully upload the file')
+                    }
+                }
+            );
+        });
+      });
+      /**
+       * 
+       * port listning
+       * 
+       * */
+      app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+      }) 
