d6aaa977a435778b44836638e626854e58e17020
Mishane Perera <44816836+mish524@users.noreply.github.com>
2021-10-03 20:28:25 +0530

    IT18198232

--- IT18198232/frontend/src/components/Home.js
+++ IT18198232/frontend/src/components/Home.js
@@ -20,7 +20,7 @@ class Home extends Component {
                   <span>MY</span> Guiders
                 </a>
               </h1>
-              <div className="nav_web-">
+              <div className="nav_web-dealingsls">
                 <nav>
                   <label htmlFor="drop" className="toggle">
                     Menu
