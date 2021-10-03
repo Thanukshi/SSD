bdb0a906c47c40bd4fa0e5f9053bb5a1f529811c
chamo000 <chamodyawanaguru96@gmail.com>
2021-10-03 17:08:13 +0530

    IT18203554

--- backend/index.js
+++ backend/index.js
@@ -1,130 +1,44 @@
-/**
- * 
- * index file to handle all requests
- * author Thenuja
- * created for SSD assignment
- * 
- * */
 
- /**
- * 
- * initialize express
- * 
- * */
   const express = require('express')
   const app = express()
-  /**
-   * 
-   * initialize port
-   * 
-   * */
+ 
   const port = 8000
-  /**
-   * 
-   * initialize body parser
-   * 
-   * */
+
   const bodyParser = require('body-parser');
-  /**
-   * 
-   * initialize cors
-   * 
-   * */
+
    const cors = require('cors');
-   /**
-    * 
-    * initialize Google APIs
-    * 
-    * */
+
    const {google} = require('googleapis');
-   /**
-    * 
-    * initialize multer
-    * 
-    * */
+ 
    const multer = require('multer');
-   /**
-    * 
-    * initialize fs
-    * 
-    * */
+
    const fs = require("fs");
-   /**
-    * 
-    * initialize formidable
-    * 
-    * */
+
    const formidable = require('formidable');
-   
-   /**
-    * 
-    * get client details from JSOM 
-    * 
-    * */
+
    const accessCredentials = require('./google_credentials/client_secret_322212537048-tp1g2qqskr5qrtk4kfulvd6mqqlqnpta.apps.googleusercontent.com.json');
-   /**
-    * 
-    * get HTTP request
-    * 
-    * */
+  
    const { request } = require('http');
-   /**
-    * 
-    * client ID
-    * 
-    * */
+
    const clientID = accessCredentials.web.client_id;
-   /**
-    * 
-    * client secret key
-    * 
-    * */
+ 
    const clientSecret = accessCredentials.web.client_secret;
-   /**
-    * 
-    * redirect URI
-    * 
-    * */
+
    const redirectURIs = accessCredentials.web.redirect_uris;
-   /**
-    * 
-    * OAuth client
-    * 
-    * */
+
    const oAuth2Client = new google.auth.OAuth2(clientID,clientSecret,redirectURIs[0]);
-   
-   /**
-    * 
-    * scopes
-    * read meta data in drive
-    * access user profile
-    * upload files or images
-    * 
-    * */
+
    const ACCESS_SCOPE = ['https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file'] 
-   /**
-    * 
-    * get output as a JSON format
-    * 
-    * */
+
     app.use(bodyParser.urlencoded({extended:false}));
     app.use(bodyParser.json());
     app.use(cors());
-    
-    /**
-     * 
-     * get request
-     * 
-     * */
+
     app.get('/', (req, res) => {
       res.send('Back end api running')
     });
     
-    /**
-     * 
-     * get authentication URL
-     * 
-     * */
+
      app.get('/getAuthURL',(req,res)=>{
         const authURL = oAuth2Client.generateAuthUrl({
             access_type:'offline',
@@ -133,13 +47,7 @@
         console.log(authURL);
         return res.send(authURL);
     });
-    
-    /**
-     * 
-     * get access token
-     * @param access code
-     * 
-     * */
+
      app.post('/getToken',(req,res)=>{
         if(req.body.code == null) return res.status(400).send('Invalid Request');
         oAuth2Client.getToken(req.body.code, (err,token)=>{
@@ -151,12 +59,7 @@
         });
       });
       
-      /**
-       * 
-       * access the user profile
-       * @param token
-       * 
-       * */
+
       app.post('/getUserInfo', (req, res) => {
         if (req.body.token == null) return res.status(400).send('Token not found');
         oAuth2Client.setCredentials(req.body.token);
@@ -169,13 +72,7 @@
         })
       });
       
-      /**
-       * 
-       * access file from drive & view
-       * @param token
-       * @returns files
-       * 
-       * */ 
+      
       app.post('/readDrive', (req, res) => {
         if (req.body.token == null) return res.status(400).send('Token not found');
         oAuth2Client.setCredentials(req.body.token);
@@ -199,15 +96,7 @@
             res.send(files);
         });
       });
-      
-      /**
-       * 
-       * file upload to drive
-       * @param files
-       * @param token
-       * @returns message
-       * 
-       * */
+
       app.post('/fileUpload', (req, res) => {
         var form = new formidable.IncomingForm();
         form.parse(req, (err, fields, files) => {
@@ -243,11 +132,9 @@
             );
         });
       });
-      /**
-       * 
-       * port listning
-       * 
-       * */
+  
       app.listen(port, () => {
         console.log(`Example app listening at http://localhost:${port}`)
-      }) 
+      })   
+      
+      cdsssdfsdf
