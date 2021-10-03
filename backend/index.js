451319cff03e85c5e0d8ae12299fa7f18e90082c
Thanukshi Wickramasinghe <43212582+Thanukshi@users.noreply.github.com>
2021-10-03 12:01:14 +0530

    IT18202632

--- backend/index.js
+++ backend/index.js
@@ -30,4 +30,81 @@
    * initialize cors
    * 
    * */
+   const cors = require('cors');
+   /**
+    * 
+    * initialize Google APIs
+    * 
+    * */
+   const {google} = require('googleapis');
+   /**
+    * 
+    * initialize multer
+    * 
+    * */
+   const multer = require('multer');
+   /**
+    * 
+    * initialize fs
+    * 
+    * */
+   const fs = require("fs");
+   /**
+    * 
+    * initialize formidable
+    * 
+    * */
+   const formidable = require('formidable');
+   
+   /**
+    * 
+    * get client details from JSOM 
+    * 
+    * */
+   const accessCredentials = require('./google_credentials/client_secret_322212537048-tp1g2qqskr5qrtk4kfulvd6mqqlqnpta.apps.googleusercontent.com.json');
+   /**
+    * 
+    * get HTTP request
+    * 
+    * */
+   const { request } = require('http');
+   /**
+    * 
+    * client ID
+    * 
+    * */
+   const clientID = accessCredentials.web.client_id;
+   /**
+    * 
+    * client secret key
+    * 
+    * */
+   const clientSecret = accessCredentials.web.client_secret;
+   /**
+    * 
+    * redirect URI
+    * 
+    * */
+   const redirectURIs = accessCredentials.web.redirect_uris;
+   /**
+    * 
+    * OAuth client
+    * 
+    * */
+   const oAuth2Client = new google.auth.OAuth2(clientID,clientSecret,redirectURIs[0]);
+   
+   /**
+    * 
+    * scopes
+    * read meta data in drive
+    * access user profile
+    * upload files or images
+    * 
+    * */
+   const ACCESS_SCOPE = ['https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file'] 
+   /**
+    * 
+    * get output as a JSON format
+    * 
+    * */
   
