edf04549712bf227ae94c57c332d52da2ac7b550
Ravidi-rd <rathnasriravidi@gmail.com>
2021-10-03 20:43:43 +0530

    IT18214376

--- It18214376/frontend/src/components/Login.js
+++ It18214376/frontend/src/components/Login.js
@@ -49,7 +49,7 @@ class Home extends Component {
                             Services
                           </a>                         
                         </li>
-                        <li>ddddddd
+                        <li>dddddd
                     
                           <a className="drop-text" href="about.html">
                             Our Chefs
